8984aa07c59f174a0faa91f3733a8c8ae86fc8b1
Mahipal Singh Jhala <115163061+MahipalSinghJhala707@users.noreply.github.com>
2024-04-10 22:37:38 +0530

    Add files via upload

---
+++ for_each_loop.java
@@ -0,0 +1,8 @@
+public class for_each_loop {
+    public static void main(String[] args) {
+        String[] animals = {"cat", "dog", "cow", "horse"};
+        for (String i : animals) {
+            System.out.println(i);
+        }
+    }
+}
---
+++ stringmethods.java
@@ -0,0 +1,15 @@
+public class stringmethods {
+    public static void main(String[] args){
+        String name = "     Bro";
+        // int result = name.length();
+        // Boolean result = name.equals("bro");
+        // Boolean result = name.equalsIgnoreCase("bro");
+        // char result = name.charAt(1);
+        // int result = name.indexOf('r');
+        // String result = name.toLowerCase();
+        // String result = name.toUpperCase();
+        String result = name.trim();
+        System.out.println(result);
+    }
+    
+}
