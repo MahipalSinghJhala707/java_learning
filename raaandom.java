e95763fd21adf3c6de1ca4272e2da8de9790f14f
Mahipal Singh Jhala <115163061+MahipalSinghJhala707@users.noreply.github.com>
2024-04-22 22:54:59 +0530

    Add files via upload

---
+++ raaandom.java
@@ -0,0 +1,9 @@
+import java.util.Random;
+public class raaandom {
+    public static void main(String[] args){
+        Random random = new Random();
+        int x = random.nextInt(1,6);
+        System.out.println(x);
+    }
+}
+
