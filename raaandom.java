75053aee8ade40cb95efbcf84a4c9ef3fa526ac6
mahipal <mahipalsinghjhala707@gmail.com>
2024-05-22 22:23:34 +0530

    lab

--- raaandom.java
+++ raaandom.java
@@ -2,8 +2,8 @@ import java.util.Random;
 public class raaandom {
     public static void main(String[] args){
         Random random = new Random();
-        int x = random.nextInt(1,6);
-        System.out.println(x);
+        int x = random.nextInt(6);
+        System.out.println(x+1);
     }
 }
 
